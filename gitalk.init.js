18a1505a3413fe291a29c0652f11ff24d35c803d
杨玉峰 <yangyufeng0423@qq.com>
2021-02-07 17:14:30 +0800

    初始化issues

--- gitalk.init.js
+++ gitalk.init.js
@@ -5,22 +5,29 @@ const url = require("url");
 const xmlParser = require("xml-parser");
 const YAML = require("yamljs");
 const cheerio = require("cheerio");
+// 获取网站配置
+const websiteConfig = YAML.parse(fs.readFileSync(path.resolve(__dirname, "./_config.yml"), "utf8"));
 // 根据自己的情况进行配置
 const config = {
-  username: "yangyfeng", // GitHub 用户名
+  // GitHub 用户名
+  username: "yangyfeng",
   // GitHub Token
-  token: "a9379c45626a2bcc2bb25c9f29d9df9f2d6345d0",
-  repo: "yangyfeng.github.io", // 存放 issues的git仓库
+  token: "f2b57a04eb80228fcdd44ee22d01be12faf0b912",
+  // 存放 issues的git仓库
+  repo: "yangyfeng.github.io",
   // sitemap.xml的路径，commit.js放置在根目录下，无需修改，其他情况自行处理
-  sitemapUrl: path.resolve(__dirname, "./public/sitemap.xml"),
-  kind: "Gitalk", // "Gitalk" or "Gitment"
+  sitemapUrl: path.resolve(__dirname, `./${websiteConfig.public_dir}/sitemap.xml`),
+  // "Gitalk" or "Gitment"
+  kind: "Gitalk",
 };
 let issuesUrl = `https://api.github.com/repos/${config.username}/${config.repo}/issues?access_token=${config.token}`;
 let requestGetOpt = {
-  url: `${issuesUrl}&page=1&per_page=100000`,
+  method: "GET",
+  url: `${issuesUrl}&page=1&per_page=100&state=all`,
   json: true,
   headers: {
-    "User-Agent": "github-user"
+    "User-Agent": "github-user",
+    accept: "application/vnd.github.v3+json"
   }
 };
 let requestPostOpt = {
@@ -33,11 +40,11 @@ console.log("开始初始化评论...");
 (async function () {
   console.log("开始检索链接，请稍等...");
   try {
-    let websiteConfig = YAML.parse(fs.readFileSync(path.resolve(__dirname, "./_config.yml"), "utf8"));
     let urls = sitemapXmlReader(config.sitemapUrl);
     console.log(`共检索到${urls.length}个链接`);
     console.log("开始获取已经初始化的issues...");
-    let issues = await send(requestGetOpt);
+    const issues = await send(requestGetOpt);
+    debugger
     if (issues.length) {
       console.log(`已经存在${issues.length}个issues`)
     } else {
@@ -78,38 +85,30 @@ console.log("开始初始化评论...");
       }
     }
     console.log('开始初始化issue...')
-    if (notInitIssueLinks.length > 0) {
-      console.log(`本次有${notInitIssueLinks.length}个链接需要初始化issue：`, notInitIssueLinks);
-      console.log("开始提交初始化请求, 大约需要40秒...");
-      /**
-       * 部署好网站后，直接执行start，新增文章并不会生成评论
-       * 经测试，最少需要等待40秒，才可以正确生成， 怀疑跟github的api有关系，没有找到实锤
-       */
-      setTimeout(async () => {
-        let initRet = await notInitIssueLinks.map(async (item) => {
-          let html = await send({
-            ...requestGetOpt,
-            url: item
-          });
-          let title = cheerio.load(html)("title").text();
-          let pathLabel = url.parse(item).path;
-          let body = `${item}<br><br>${websiteConfig.description}`;
-          let form = JSON.stringify({
-            body,
-            labels: [config.kind, pathLabel],
-            title
-          });
-          return send({
-            ...requestPostOpt,
-            form
-          });
-        });
-        console.log(`已完成${initRet.length}个！`);
-        console.log("可以愉快的发表评论了！");
-      }, 40000);
-    } else {
+    if (notInitIssueLinks.length === 0) {
       console.log("本次发布无新增页面，无需初始化issue!!");
+      return
     }
+    console.log(`本次有${notInitIssueLinks.length}个链接需要初始化issue：`, notInitIssueLinks);
+    notInitIssueLinks.forEach(async (item) => {
+      let html = await send({
+        ...requestGetOpt,
+        url: item
+      });
+      let title = cheerio.load(html)("title").text();
+      let pathLabel = url.parse(item).path;
+      let body = `${item}<br><br>${websiteConfig.description}`;
+      let form = JSON.stringify({
+        body,
+        labels: [config.kind, pathLabel],
+        title
+      });
+      send({
+        ...requestPostOpt,
+        form
+      })
+    });
+    console.log("可以愉快的发表评论了！");
   } catch (e) {
     console.log('检索失败')
   }
